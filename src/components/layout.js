ed70f3cde5d05bcab619d531d6c3a4c9f3429598
Adham El Banhawy <the.benhawy@gmail.com>
2021-01-22 12:05:41 +0200

    used custom theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,25 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import "./styles.css"
+import 'fontsource-roboto';
+import { Paper } from "@material-ui/core"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#7EDDD3",
+      main: "#81D8D0",
+      dark: "#34AB9F"
+    },
+    secondary: {
+      main: "#D9828B"
+    },
+    // type: 'dark'
+  }
+})
 
 const Layout = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -20,17 +38,19 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header
-        siteTitle={data.site.siteMetadata.title}
-        menuOpen={menuOpen}
-        setMenuOpen={setMenuOpen}
-      />
-      <div className="container">
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+        />
+        <div className="container">
+          <main>{children}</main>
+        </div>
+        <Footer />
+      </Paper>
+    </ThemeProvider>
   )
 }
 
